e5702e0eb0519f6b8a7313ecc132f8f928e04fe2
Srinivas <srininara@gmail.com>
2014-08-14 21:25:06 +0530

    script for loading data from csv using the API

---
+++ db_loader/loader.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import csv
+import requests
+import json
+from datetime import datetime
+
+# Slno,Date,Description,Amount,Tag1,Tag2
+expense_list_API_url = "http://localhost:5000/grihasthi/api/v1.0/expenses"
+
+
+def convert(value):
+    return (datetime.strptime(value, "%d-%b-%Y")).strftime("%Y-%m-%d")
+
+
+def post_expenses(date, description, amount, tag1, tag2):
+  # print(date)
+  payload = {'description':description, 'expense_date':date, 'amount':amount, 'tags': [{"name":tag1},{"name":tag2}]}
+  headers = {'content-type': 'application/json'}
+
+  r = requests.post(expense_list_API_url,data=json.dumps(payload),headers=headers)
+  if 201!=r.status_code:
+    raise RuntimeError("insert failed")
+
+with open('DailyExpenseTracker.csv','r') as f:
+  reader = csv.DictReader(f,delimiter=',')
+  count = 0
+  for row in reader:
+    if row['Date'] == "":
+      break
+    post_expenses(convert(row['Date']), row['Description'], row['Amount'], row['Tag1'], row['Tag2'])
+
+
+# if __name__ == '__main__':
+#   unittest.main()
