13d4473cada2a0dcd4e3974ee8b03e44c88e727b
Srinivas <srininara@gmail.com>
2014-08-26 14:21:40 +0530

    Updated tags test to work with the new model

--- api_func_tests/test_api_expenses.py
+++ api_func_tests/test_api_expenses.py
@@ -30,19 +30,24 @@ class TestExpensesAPI(unittest.TestCase):
     self.assertIsNotNone(tags)
     self.assertEqual(len(tags),0)
 
-# def test_post_expenses_with_non_existant_category(self):
+  # def test_post_expenses_with_non_existant_category(self):
 
-  # def test_post_expenses_with_tags(self):
-  #   payload = {'description':'test expense with tag', 'expense_date':'2014-06-29', 'amount':'110.10', 'tags': [{"name":"testTag1"},{"name":"testTag2"}]}
-  #   headers = {'content-type': 'application/json'}
-  #
-  #   r = requests.post(self.expense_list_API_url,data=json.dumps(payload),headers=headers)
-  #   self.assertEqual(201,r.status_code)
-  #   output = r.json()
-  #   self.assertTrue(output.get('id',-1)!=-1)
-  #   tags = output.get('tags')
-  #   self.assertIsNotNone(tags)
-  #   self.assertEqual(len(tags),2)
+  def test_post_expenses_with_tags(self):
+    payload = {'description':'test expense with tag', 'expense_date':'2014-06-29', 'amount':'110.10', 'tags': [{"name":"testTag1"},{"name":"testTag2"}], 'category': 'Apparel', 'subcategory': 'Regular Wear', 'nature': 'Necessity', 'frequency': 'Regular'}
+    headers = {'content-type': 'application/json'}
+
+    r = requests.post(self.expense_list_API_url,data=json.dumps(payload),headers=headers)
+    self.assertEqual(201,r.status_code)
+    output = r.json()
+    self.assertTrue(output.get('id',-1)!=-1)
+    self.assertTrue(output.get('nature_id',-1)!=-1)
+    self.assertTrue(output.get('category_id',-1)!=-1)
+    self.assertTrue(output.get('subcategory_id',-1)!=-1)
+    self.assertTrue(output.get('frequency_id',-1)!=-1)
+
+    tags = output.get('tags')
+    self.assertIsNotNone(tags)
+    self.assertEqual(len(tags),2)
 
   # def test_postExpenses_without_description(self):
   #   payload = {'expense_date':'2014-06-29', 'amount':'120.10'}
