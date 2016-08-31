a8312848d5f2261251289ac5dc4e32da323c1629
Srinivas Narayanan <srininara@gmail.com>
2016-09-01 03:15:14 +0530

    direct reference to path
    Direct reference to gunicorn installed script inside the shell script

--- bin/ayavyaya_g_starter.sh
+++ bin/ayavyaya_g_starter.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-gunicorn -b 0.0.0.0:8000 -w 1 app:app -k gevent \
+/usr/share/python/ayavyaya/bin/gunicorn -b 0.0.0.0:8000 -w 1 ayavyaya:app -k gevent \
     --error-logfile ./logs/gunicorn/app/app.log \
     --log-file ./logs/gunicorn/app/app.log \
     --access-logfile ./logs/gunicorn/access/access.log \
